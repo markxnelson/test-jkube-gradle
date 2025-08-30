539c735aede72215d93ed618a406b76485764913
Mark Nelson <mark.x.nelson@oracle.com>
2025-08-30 10:09:28 -0400

    starting point
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
